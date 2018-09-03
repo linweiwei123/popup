ae5aaf6384519371f9c5190c751faa70c17bdb56
林伟伟 <linweiwei@xiaoyouzi.com>
2018-09-03 11:35:14 +0800

    message

--- src/popup.js
+++ src/popup.js
@@ -3,159 +3,230 @@ require('./popup.css');
 var is = function (obj, type) { return Object.prototype.toString.call(obj).toLowerCase() === ("[object " + type + "]"); };
 
 var addClass = function (el, cls) {
-  var arr = el.className
-    .split(/\s+/)
-    .filter(function (c) { return !!c && c === cls; });
+    var arr = el.className
+        .split(/\s+/)
+        .filter(function (c) { return !!c && c === cls; });
 
-  if (!arr.length) {
-    el.className += " " + cls;
-  }
+    if (!arr.length) {
+        el.className += " " + cls;
+    }
 };
 
 var removeClass = function (el, cls) {
-  el.className = el.className
-    .split(/\s+/)
-    .filter(function (c) { return !!c && c !== cls; })
-    .join(' ');
+    el.className = el.className
+        .split(/\s+/)
+        .filter(function (c) { return !!c && c !== cls; })
+        .join(' ');
 };
 
 var Popup = function Popup(el, options){
-   let defaultOpts = {
-     container: 'body', // 页面容器
-     header: 'center', // header 位置样式left, center
-     animate: false,
-     popupSelector: 'meet-popup-container',
-     popupCloseSelector: 'popup-close-icon',
-     popupMaskSelector: 'meet-popup-mask',
-     popupCloseClass: 'meet-popup-close',
-     popupOpenClass: 'meet-popup-open',
-     popupSlideUp: 'meet-popup-bounce-up',
-     popupSlideDown: 'meet-popup-slide-down'
-   };
+    var _this = this;
+    var defaultOpts = {
+        container: 'body', // 页面容器
+        header: 'center', // header 位置样式left, center
+        animate: false,
+        popupSelector: 'meet-popup-container',
+        popupCloseSelector: 'p-right',
+        popupMaskSelector: 'meet-popup-mask',
+        popupCloseClass: 'meet-popup-close',
+        popupOpenClass: 'meet-popup-open',
+        popupSlideUp: 'meet-popup-bounce-up',
+        popupSlideDown: 'meet-popup-slide-down'
+    };
 
-   let opts = Object.assign(defaultOpts, options || {});
+    var opts = Object.assign(defaultOpts, options || {});
 
-   opts.popupSlideUp = opts.animate? 'meet-popup-bounce-up': 'meet-popup-bounce-up';
-   opts.popupSlideDown = opts.animate? 'meet-popup-bounce-down': 'meet-popup-slide-down';
+    opts.popupSlideUp = opts.animate? opts.popupSlideUp: '';
+    opts.popupSlideDown = opts.animate? opts.popupSlideDown: '';
 
-   this.opts = {};
+    this.opts = {};
 
-   Object.keys(opts)
-     .forEach((key)=>{
-       this.opts[key] = opts[key];
-     });
+    Object.keys(opts)
+        .forEach(function(key){
+            _this.opts[key] = opts[key];
+        });
 
-   this.wrapper = el;
-   this.docContainer = document.querySelector(this.opts.container);
-   this.popup = document.querySelector('.' + this.opts.popupSelector);
-   this.mask = document.querySelector('.' + this.opts.popupMaskSelector);
+    this.wrapper = el;
+    this.popup = el.querySelector('.' + this.opts.popupSelector);
+    this.mask = el.querySelector('.' + this.opts.popupMaskSelector);
 
-   // header 样式
-   let headerEl = document.querySelector('.popup-header');
-   headerEl.classList.add(this.opts.header);
+    // header 样式
+    var headerEl = el.querySelector('.popup-header');
+    headerEl.classList.add(this.opts.header);
 
-   this.wrapper.querySelector('.' + this.opts.popupCloseSelector).addEventListener('click', () => {
-     this.close();
-   });
+    this.wrapper.querySelector('.' + this.opts.popupCloseSelector).addEventListener('click', function(){
+        _this.close();
+    });
 
-   headerEl.addEventListener('touchmove', function(e){
-     e.preventDefault();
-     e.stopPropagation();
-     return false;
-   },false);
+    this.mask.addEventListener('click',function(){
+        _this.close();
+    });
 
-  this.mask.addEventListener('touchmove', function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    return false;
-  },false)
+    scrollPrevent(el);
 };
 
 Popup.prototype.open = function(){
-  if(!is(this.opts.beforeOpen,'function')){
-    return this._doOpen();
-  }
+    var _this = this;
+    if(!is(this.opts.beforeOpen,'function')){
+        return this._doOpen();
+    }
 
-  this.opts.beforeOpen(()=>{
-    this._doOpen();
-  });
+    this.opts.beforeOpen(function(){
+        _this._doOpen();
+    });
 
 };
 
 Popup.prototype._doOpen = function(){
-  // clear close styes
-  removeClass(this.wrapper, this.opts.popupCloseClass);
-  removeClass(this.popup, this.opts.popupSlideDown);
-  removeClass(this.mask, 'fade-out');
+    var _this = this;
+    // clear close styes
+    removeClass(this.wrapper, this.opts.popupCloseClass);
+    removeClass(this.popup, this.opts.popupSlideDown);
+    removeClass(this.mask, 'fade-out');
 
-  // 显示
-  addClass(this.wrapper, this.opts.popupOpenClass);
-  this.lockBody();
-  // 上滑动画
-  addClass(this.popup,this.opts.popupSlideUp);
+    // 显示
+    addClass(this.wrapper, this.opts.popupOpenClass);
+    this.lockBody();
+    // 上滑动画
+    addClass(this.popup,this.opts.popupSlideUp);
 
-  setTimeout(()=>{
-    if(is(this.opts.afterOpen,'function')){
-      return this.opts.afterOpen();
-    }
-  },300);
+    setTimeout(function(){
+        if(is(_this.opts.afterOpen,'function')){
+            return _this.opts.afterOpen();
+        }
+    },300);
 
 };
 
 Popup.prototype.close = function(){
+    var _this = this;
+    if(!is(this.opts.beforeClose,'function')){
+        return this._doClose();
+    }
 
-  if(!is(this.opts.beforeClose,'function')){
-    return this._doClose();
-  }
-
-  this.opts.beforeClose(()=>{
-    this._doClose();
-  })
+    this.opts.beforeClose(function(){
+        _this._doClose();
+    })
 };
 
 Popup.prototype._doClose = function(){
-  // clear open styles
-  removeClass(this.popup, this.opts.popupSlideUp);
+    var _this = this;
 
-  // 下滑动画
-  addClass(this.popup,this.opts.popupSlideDown);
-  // 背景淡出
-  addClass(this.mask,'fade-out');
-
-  setTimeout(()=>{
     // clear open styles
-    removeClass(this.wrapper, this.opts.popupOpenClass);
-    // close popup
-    addClass(this.wrapper, this.opts.popupCloseClass);
-    this.unlockBody();
-    if(is(this.opts.afterClose,'function')){
-      return this.opts.afterClose();
-    }
-  },300)
+    removeClass(this.popup, this.opts.popupSlideUp);
+
+    // 下滑动画
+    addClass(this.popup,this.opts.popupSlideDown);
+    // 背景淡出
+    addClass(this.mask,'fade-out');
+
+    setTimeout(function(){
+        // clear open styles
+        removeClass(_this.wrapper, _this.opts.popupOpenClass);
+        // close popup
+        addClass(_this.wrapper, _this.opts.popupCloseClass);
+        _this.unlockBody();
+        if(is(_this.opts.afterClose,'function')){
+            return _this.opts.afterClose();
+        }
+    },300)
 };
 
 // 阻止滚动穿透
 Popup.prototype.lockBody = function(){
-  if(window.pageYOffset){
-     this.scrollTop = window.pageYOffset;
-     this.docContainer.style.top = - this.scrollTop +"px";
-   }
-  this.docContainer.classList.add('overflow-hidden');
+    document.querySelector('html').classList.add('noscroll');
 };
 
 // 回复body滚动
 Popup.prototype.unlockBody = function(){
-  this.docContainer.classList.remove('overflow-hidden');
-  this.docContainer.style.top = "0px";
-  if(this.scrollTop !== null){
-    window.scrollTo(0,this.scrollTop);
-  }
-  if(this.scrollTimer){
-    clearTimeout(this.scrollTimer);
-  }
-  this.scrollTimer = window.setTimeout(()=>{
-    this.scrollTop = null;
-  }, 0);
+    document.querySelector('html').classList.remove('noscroll');
+};
+
+function scrollPrevent(popupSelector){
+    var popupContainer = popupSelector;
+    var popupContent = popupContainer.querySelector('.popup-content');
+
+    var data = {
+        maxscroll: 0
+    };
+
+    popupContainer.addEventListener('touchstart',function(event){
+        var elTarget = event.target;
+
+        if(elTarget.classList.contains('.popup-content') || getClosest(elTarget,'.popup-content') !== null){
+
+            data.scrollY = popupContent.scrollTop;
+            data.posY = (event.targetTouches[0] || event).pageY;
+            data.maxscroll = popupContent.scrollHeight - popupContent.clientHeight;
+            data.elScroll = elTarget;
+        }
+        else{
+            data.elScroll = null;
+        }
+        return;
+    });
+
+    popupContainer.addEventListener('touchmove',function(event){
+        if(!data.elScroll){
+            event.preventDefault();
+        }
+        if(event.maxscroll<=0){
+            event.preventDefault();
+        }
+
+        console.log(event.originalEvent,event);
+
+        // 现在移动的垂直位置，用来判断是往上移动还是往下
+        var events = event.targetTouches[0] || event;
+        // 当前的滚动高度
+        var scrollTop = popupContent.scrollTop;
+        console.log(events.pageY);
+        // 移动距离
+        var distanceY = events.pageY - data.posY;
+
+        // 上下边缘检测
+        if (distanceY > 0 && scrollTop == 0) {
+            // 往上滑，并且到头
+            // 禁止滚动的默认行为
+            event.preventDefault();
+            return;
+        }
+
+        // 下边缘检测
+        if (distanceY < 0 && (scrollTop + 1 >= data.maxscroll)) {
+            // 往下滑，并且到头
+            // 禁止滚动的默认行为
+            event.preventDefault();
+            return;
+        }
+    });
+
+}
+
+var getClosest = function (elem, selector) {
+
+    // Element.matches() polyfill
+    if (!Element.prototype.matches) {
+        Element.prototype.matches =
+            Element.prototype.matchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector ||
+            Element.prototype.oMatchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            function(s) {
+                var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                    i = matches.length;
+                while (--i >= 0 && matches.item(i) !== this) {}
+                return i > -1;
+            };
+    }
+
+    // Get the closest matching element
+    for ( ; elem && elem !== document; elem = elem.parentNode ) {
+        if ( elem.matches( selector ) ) return elem;
+    }
+    return null;
+
 };
 
 module.exports = Popup;
